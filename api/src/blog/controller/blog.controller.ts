5a0cb67f0e9b21e6a50d768ff8f85d7ddd091969
martinsauseklis <82760390+martinsauseklis@users.noreply.github.com>
2022-08-12 09:32:40 +0300

    added endpoints for uploading blog entry image and retrievieng them

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Post, Body, UseGuards, Request, Get, Query, Param, Put, Delete } from '@nestjs/common';
-import { from, Observable } from 'rxjs';
+import { Controller, Post, Body, UseGuards, Request, Get, Query, Param, Put, Delete, UseInterceptors, UploadedFile, Res } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { from, Observable, of } from 'rxjs';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { UserIsUserGuard } from 'src/auth/guards/UserIsUser.guard';
 import { UserIsAuthorGuard } from '../guards/user-is-author.guard';
 import { BlogEntry } from '../model/blog-entry.interface';
 import { BlogService } from '../service/blog.service';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import path = require('path');
+import { Image } from '../model/image.interface';
+import { join } from 'path';
 
 export const BLOG_ENTRIES_URL = 'http://localhost:3000/api/blog-entries';
+export const storage = {
+    storage: diskStorage({
+        destination: './uploads/blog-entry-images',
+        filename: (req, file, cb) => {
+            const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+            const extension: string = path.parse(file.originalname).ext;
+
+            cb(null, `${filename}${extension}`)
+        }
+    })
+}
 
 @Controller('blog-entries')
 export class BlogController {
@@ -72,5 +89,18 @@ export class BlogController {
     @Delete(':id')
     deleteOne(@Param('id') id: number): Observable<any> {
         return this.blogService.deleteOne(Number(id));
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('image/upload')
+    @UseInterceptors(FileInterceptor('file', storage))
+    uploadFile(@UploadedFile() file, @Request() req): Observable<Image> {
+        return of(file);
+    
+    }
+
+    @Get('image/:imagename')
+    findImage(@Param('imagename') imagename, @Res() res): Observable<Object> {
+        return of(res.sendFile(join(process.cwd(), 'uploads/blog-entry-images/'+imagename)));
     }
 }
